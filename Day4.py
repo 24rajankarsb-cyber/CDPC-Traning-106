0816d726b6ed715fd13cfbdab76f9a1604626ba9
24rajankarsb-cyber <24rajankarsb@rbunagpur.in>
2026-03-12 21:12:20 +0530

    Add files via upload

---
+++ Day4.py
@@ -0,0 +1,43 @@
+#Functions Starting with def keyword
+
+# def add():
+#     a=int(input("Enter the first number"))
+#     b=int(input("Enter the second number"))
+#     res=a+b
+#     print(res)
+# if __name__=='__main__':
+#     add()
+
+# Functions with parameters with no return value
+# def add(x,y):
+#     res=x+y
+#     print(res)
+# if __name__=='__main__':
+#     a=int(input("Enter the first number"))
+#     b=int(input("Enter the second number"))
+#     add(a,b)
+
+# Functions with parameters with return value
+
+# def add(x,y):
+#     res=x+y
+#     return res
+# if __name__=='__main__':
+#     a=int(input("Enter the first number"))
+#     b=int(input("Enter the second number"))
+#     result=add(a,b)
+#     print(res)
+
+# Multiple values return
+
+# def add(x,y):
+#     res=x+y
+#     sub=x-y
+#     mul=x*y
+#     div=x/y
+#     return res,sub,mul,div
+# if __name__=='__main__':
+#     a=int(input("Enter the first number"))
+#     b=int(input("Enter the second number"))
+#     result=add(a,b)
+#     print(result)
