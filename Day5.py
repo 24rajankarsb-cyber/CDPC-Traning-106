01ced3700c3832623228870acfdb5b787afe020a
24rajankarsb-cyber <24rajankarsb@rbunagpur.in>
2026-03-16 09:07:06 +0530

    Add files via upload

---
+++ Day5.py
@@ -0,0 +1,92 @@
+#Accept Four numbers from user and display the maximum number.
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# num3 = int(input("Enter third number: "))
+# num4 = int(input("Enter fourth number: "))
+
+# max_num = max(num1, num2, num3, num4)
+# if max_num == num1:
+#     print("The maximum number is:", num1)
+# elif max_num == num2:
+#     print("The maximum number is:", num2)
+# elif max_num == num3:
+#     print("The maximum number is:", num3)
+# else:
+#     print("The maximum number is:", num4)
+
+# Result :
+# Enter first number: 10
+# Enter second number: 20
+# Enter third number: 30
+# Enter fourth number: 40
+# Final Result: The maximum number is: 40
+
+# Accept Four numbers from user and display the minimum number.
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# num3 = int(input("Enter third number: "))
+# num4 = int(input("Enter fourth number: "))
+
+# min_num = min(num1, num2, num3, num4)
+# if min_num == num1:
+#     print("The minimum number is:", num1)
+# elif min_num == num2:
+#     print("The minimum number is:", num2)
+# elif min_num == num3:
+#     print("The minimum number is:", num3)
+# else:
+#     print("The minimum number is:", num4)
+
+# Result :
+# Enter first number: 10
+# Enter second number: 20
+# Enter third number: 30
+# Enter fourth number: 40
+# Final Result: The minimum number is: 10
+
+#Maimum and minimun array with if condition
+# arr = [9,0,1,2,3,4,5,6,7,8]
+# max_num = arr[0]
+# min_num = arr[0]
+# for num in arr:
+#     if num > max_num:
+#         max_num = num
+#     if num < min_num:
+#         min_num = num
+# print("The maximum number is:", max_num)
+# print("The minimum number is:", min_num)
+
+#Result :
+# The maximum number is: 9
+# The minimum number is: 0
+
+#Check leap year or not
+# year = int(input("Enter a year: "))
+# if year%100!=0:
+#     if year%4==0:
+#         print("Non Century Leap Year")
+#     else:
+#         print("Non Century Non-Leap Year")
+# else:
+#     if year%400==0:
+#         print("Century Leap Year")
+#     else:
+#         print("Century Non-Leap Year")
+
+#Result :
+# Enter a year: 2025
+# Non Century Non-Leap Year
+
+# Check Tech Number or Not
+# num = 2025
+# n1 = num%100
+# n2 = num//100
+# sum = n1 + n2
+# square = sum**2
+# if square == num:
+#     print("Tech Number")
+# else:
+#     print("Not a Tech Number")
+
+# Result :
+# Tech Number
